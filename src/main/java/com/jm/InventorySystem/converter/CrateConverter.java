4b607c5b3382b6fadd534bb1f669359477e4ba61
james-millner <u1257434@unimail.hud.ac.uk>
2016-03-31 15:16:14 +0100

    Added object converter to allow crate data to be sent to MongoDB.

---
+++ src/main/java/com/jm/InventorySystem/converter/CrateConverter.java
@@ -0,0 +1,39 @@
+package com.jm.InventorySystem.converter;
+
+import com.jm.InventorySystem.domain.Crate;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+import org.bson.types.ObjectId;
+
+import java.util.Date;
+
+/**
+ * Created by James on 31/03/2016.
+ */
+public class CrateConverter {
+
+    //Converts a crate into a DB Object.
+    public static DBObject toDBObject(Crate crate) {
+        BasicDBObjectBuilder builder = BasicDBObjectBuilder.start().append(
+                "cName", crate.getcName()).append(
+                "width", crate.getWidth()).append(
+                "height", crate.getHeight()).append(
+                "dateCreated", crate.getDateCreated());
+        if(crate.getId() != null) {
+            builder = builder.append("_id", new ObjectId(crate.getId()));
+        }
+        return builder.get();
+    }
+
+    public static Crate toCrate(DBObject obj) {
+        Crate c = new Crate();
+        ObjectId id = (ObjectId) obj.get("_id");
+        c.setId(id.toString());
+        c.setcName((String) obj.get("cName"));
+        c.setWidth((Integer) obj.get("width"));
+        c.setHeight((Integer) obj.get("height"));
+        c.setDateCreated((Date) obj.get("dateCreated"));
+        return  c;
+    }
+
+}
