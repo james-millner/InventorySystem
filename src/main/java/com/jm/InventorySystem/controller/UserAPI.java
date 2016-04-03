ba8db3fc0efdc987b691ce613cb6ec1c048a876c
james-millner <u1257434@unimail.hud.ac.uk>
2016-04-03 15:28:16 +0100

    Added very basic API to get user details, based on a username parameter. Returns the matching JSON Data.

---
+++ src/main/java/com/jm/InventorySystem/controller/UserAPI.java
@@ -0,0 +1,30 @@
+package com.jm.InventorySystem.controller;
+
+import com.jm.InventorySystem.DAO.MongoDBUserDAO;
+import com.jm.InventorySystem.converter.UserConverter;
+import com.jm.InventorySystem.domain.User;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by James on 01/04/2016.
+ */
+
+@RestController
+public class UserAPI {
+
+    @RequestMapping(value = "/api")
+    public DBObject user(@RequestParam("name") String name) {
+        // Since 2.10.0, uses MongoClient
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        MongoDBUserDAO userDAO = new MongoDBUserDAO(mongo);
+        User user = new User();
+        user.setUsername(name);
+        User got = userDAO.getUser(user);
+        return UserConverter.toDBObject(got);
+    }
+}
